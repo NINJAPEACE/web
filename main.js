e1afee9a2a6a9e7e1ca6c100db0ec8fefb99343b
kenzie.roozen0304@gmail.com <kenzie.roozen0304@gmail.com>
2022-12-20 16:17:49 +0700

    v1.4

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-var hamburger = document.querySelector(".hamb");
-var navList = document.querySelector(".nav-list");
-var links = document.querySelector(".nav-list li")
+var who = document.querySelector(".btn");
 
-hamburger.addEventListener("click", function() {
-  this.classList.toggle("click");
-  navList.classList.toggle("open");
-})
+who.addEventListener("click", function() {
+  
+});
+// <div class="easter"></div>
