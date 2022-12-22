508b4b3835ba2b032bc6c1d814514aff3c67d50b
kenzie.roozen0304@gmail.com <kenzie.roozen0304@gmail.com>
2022-12-22 13:17:53 +0700

    v1.6

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-var who = document.querySelector(".btn");
-
-who.addEventListener("click", function() {
+var more = document.querySelector(".more");
+var list = [
+  "No, you can not", 
+  "Stop clicking", 
+  "Bro, please stop", 
+  "No way", 
+  "LMAO", 
+  "Who are you?", 
+  "Dude, get some help", 
+  "Get out", 
+  "I am tired of this", 
+  "Do not do that again", 
+  "I am warning you", 
+  "S.T.O.P.", 
+  "System Error!", 
+  "Server Error!", 
+  " Reconnecting to the server...", 
+  "What?"];
   
+const listB = list;
+
+more.addEventListener("click", function() {
+  let many = this.getAttribute("many");
+  let newText = list[Math.floor(Math.random() * list.length)];
+
+  if (many <= 0) this.innerText = "I am kidding";
+  else if(!newText) this.innerText = "More about me", many = -1, list = listB;
+  else this.innerText = newText;
+
+  this.setAttribute("many", parseInt(many) + 1);
+  list = list.filter(e => e !== newText);
 });
-// <div class="easter"></div>
