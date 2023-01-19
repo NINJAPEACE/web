8ee8c25efdedde3f9825e51a60a1df9dc01ae19e
kenzie.roozen0304@gmail.com <kenzie.roozen0304@gmail.com>
2023-01-19 12:49:37 +0700

    v1.7

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-var more = document.querySelector(".more");
+/*ar more = document.querySelector(".more");
 var list = [
-  "No, you can not", 
-  "Stop clicking", 
-  "Bro, please stop", 
-  "No way", 
-  "LMAO", 
-  "Who are you?", 
-  "Dude, get some help", 
-  "Get out", 
-  "I am tired of this", 
-  "Do not do that again", 
-  "I am warning you", 
-  "S.T.O.P.", 
-  "System Error!", 
-  "Server Error!", 
-  " Reconnecting to the server...", 
+  "No, you can not",
+  "Stop clicking",
+  "Bro, please stop",
+  "No way",
+  "LMAO",
+  "Who are you?",
+  "Dude, get some help",
+  "Get out",
+  "I am tired of this",
+  "Do not do that again",
+  "I am warning you",
+  "S.T.O.P.",
+  "System Error!",
+  "Server Error!",
+  " Reconnecting to the server...",
   "What?"];
-  
+
 const listB = list;
 
 more.addEventListener("click", function() {
@@ -24,9 +24,32 @@ more.addEventListener("click", function() {
   let newText = list[Math.floor(Math.random() * list.length)];
 
   if (many <= 0) this.innerText = "I am kidding";
-  else if(!newText) this.innerText = "More about me", many = -1, list = listB;
+  else if (!newText) this.innerText = "More about me", many = -1, list = listB;
   else this.innerText = newText;
 
   this.setAttribute("many", parseInt(many) + 1);
   list = list.filter(e => e !== newText);
 });
+*/
+
+function swipeActive() {
+  var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    }
+  });
+}
+
+swipeActive();
