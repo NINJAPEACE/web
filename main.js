71fe66401f44e7afffff8a5b6ff42e7d2ca62bd5
kenzie.roozen0304@gmail.com <kenzie.roozen0304@gmail.com>
2023-04-07 09:17:04 +0700

    v1.8.3.0-development

--- main.js
+++ main.js
@@ -16,40 +16,70 @@ function swipeActive() {
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
+    }, 
+    on: {
+      slideChange: (s) => {
+        let arr = {
+          "0": "one",
+          "1": "two",
+          "2": "three",
+          "3": "four",
+          "4": "five"
+        }
+        
+        let page = document.querySelector(`.${arr[s.realIndex]}`);
+
+        //removing the latest page
+        if (current.index !== s.realIndex) {
+          current.page.removeAttribute("status");
+        }
+        
+        page.setAttribute("status", "active");
+        current = { index: s.realIndex, page: page };
+      }
     }
   });
 }
 
 swipeActive();
 
-var current = { index: 1, page: document.querySelector(".one") };
+var current = { index: 0, page: document.querySelector(".one") };
 
 document.querySelector(".one").setAttribute("status", "active")
 
+/*
 swiper.on("slideChange", function() {
+  
+  console.log(swiper.activeIndex)
+  
+  let index = swiper.activeIndex == 0 ? 6 : swiper.activeIndex == 7 ? 1 : swiper.activeIndex;
 
   let arr = {
-    "1": "one",
-    "2": "two",
-    "3": "three",
-    "4": "four",
-    "5": "five",
-    "6": "six"
+    "0": "one",
+    "1": "two",
+    "2": "three",
+    "3": "four",
+    "4": "five",
+    "5": "six"
   }
 
-  let page = document.querySelector(`.${arr[swiper.activeIndex]}`);
+  let page = document.querySelector(`.${arr[index]}`);
 
   //removing the latest page
-  if (current.index !== swiper.activeIndex) {
+  if (current.index !== index) {
     current.page.removeAttribute("status");
   }
 
   page.setAttribute("status", "active");
-  current = { index: swiper.activeIndex, page: page };
-  
-  document.querySelectorAll(".item").forEach(e => e.removeAttribute("s"));
+  current = { index: index, page: page };
+
+  setTimeout(() => {
+    document.querySelectorAll(".item").forEach(e => e.removeAttribute("s"));
+  }, 500);
 });
 
 swiper.on("sliderMove", function(swiper, event) {
   document.querySelectorAll(".item").forEach(e => e.setAttribute("s", "onchange"));
-});
+});*/
+
+//alert("WARNING! \n\nThis Website is currently under development, all features here hasn't responsible by the developer.");
